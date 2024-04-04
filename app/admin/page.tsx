b827ddbcc8b89883d2927071bff51887caa6ab1e
Blank-FS <137102716+Blank-FS@users.noreply.github.com>
2024-04-03 22:47:35 -0700

    removed unused files

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Display from "@/components/displays/display";
 import AdminDisplay from "@/components/admin/admin-display";
 // Main page //
 export default function Page() {
