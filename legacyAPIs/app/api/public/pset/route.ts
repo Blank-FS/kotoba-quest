fd59462f8d1bdfd89eae6ab47e8a48a6538e7f74
Blank-FS <137102716+Blank-FS@users.noreply.github.com>
2024-11-07 19:03:36 -0500

    moved legacy apis to separate folder

---
+++ legacyAPIs/app/api/public/pset/route.ts
@@ -0,0 +1,32 @@
+import { NextRequest, NextResponse } from "next/server";
+// import prisma from "@/prisma/db";
+import { wordPset } from "@/lib/pset-generators/word-pset";
+import { dataMap } from "@/constants/staticData";
+
+// GET REQUEST
+export async function GET(req: NextRequest) {
+  const { searchParams } = req.nextUrl;
+  try {
+    const amount = searchParams.get("amount");
+    const type = searchParams.get("type");
+    //Getting data
+    let data = type ? dataMap.get(type) : dataMap.get("noun");
+    /*
+    type
+      ? (data = await prisma.word.findMany({
+          where: { Group: { Type: { type: type } } },
+        }))
+      : (data = await prisma.word.findMany());
+    */
+
+    // Creating pset
+    let pset;
+    amount
+      ? (pset = wordPset(parseInt(amount, 10), data))
+      : (pset = wordPset(1, data));
+    return NextResponse.json({ pset });
+  } catch (error) {
+    console.error("Error getting pset:", error);
+    return NextResponse.json({ error });
+  }
+}
